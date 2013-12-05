9b2e58713b2db39754f958927aaa5f6fda313709
Michael MacCallum <maccdevteam@gmail.com>
2013-12-05 08:37:36 -0500

    Changed regular loops to generator expressions. Seems to have shaved about a 0.02 average off

--- RiemannSums.py
+++ RiemannSums.py
@@ -2,56 +2,52 @@ import time
 
 def leftRiemann(f,a,n,h):
 		
-	sum = 0.0	
-	
-	for i in xrange(n):
-		x = a + i * h
-		sum += f(x)
-		
-	sum *= h
+	t = sum((f(i * h + a)) for i in xrange(n))
 
-	return sum
+	
+	# for i in xrange(n):
+	# 	x = a + i * h
+	# 	t += f(x)
+		
+	t *= h
+
+	return t
 	
 	
 def rightRiemann(f,a,n,h):
 	
-	sum = 0.0	
+	t = sum((f(i * h + a)) for i in xrange(1, n + 1))
 
-	for i in xrange(1, n + 1):
-		x = a + i * h
-		sum += f(x)
-	sum *= h
+	t *= h
 	
-	return sum
+	return t
 
 def middleRiemann(f,a,n,h):
 
-	sum = 0.0
 	add = h * 0.5 + a
 
-	for i in xrange(n):
-		x = i * h + add
-		sum += f(x)
-	sum *= h
+	t = sum((f(i * h + add)) for i in xrange(n))
 
-	return sum
+	t *= h
+
+	return t
 
 def trapezoidRiemann(f,a,b,n,h):
 	
-	sum = f(a) + f(b)
+	s = f(a) + f(b)
 
-	for i in xrange(1, n):
-		x = i * h + a
-		sum += (f(x) * 2.0)
-	sum *= (h * 0.5)
+	t = sum((2.0 * f(i * h + a)) for i in xrange(1, n)) + s
 
-	return sum
+	t *= (h * 0.5)
+
+	return t
+
 
 f = lambda x:5*x
 
 a = 0.0
 b = 100.0
-n = 20000
+n = 250000
 h = (b - a) / float(n)
 
 start = time.time()
