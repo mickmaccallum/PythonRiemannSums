0e8f6af182e7b1272933fb076bf415a799304c85
Michael MacCallum <maccdevteam@gmail.com>
2013-12-09 11:01:44 -0500

    Added some profiling to left summation method

--- RiemannSums.py
+++ RiemannSums.py
@@ -1,7 +1,14 @@
 import time
 
 def left_riemann(f,a,n,h):
-		
+		# print h		
+		# s = 0.0
+		# for i in xrange(n):
+		# 	g = f(i * h + a)
+		# 	print g
+		# 	s += g
+		# s *= h
+		# return s	
 	return sum((f(i * h + a)) for i in xrange(n)) * h
 	
 	
@@ -30,7 +37,7 @@ def trapezoid_riemann(f,a,b,n,h):
 
 f = lambda x:5*x
 
-a = 0.0
+a = -3.0
 b = 100.0
 n = 20000
 h = (b - a) / float(n)
@@ -38,9 +45,20 @@ h = (b - a) / float(n)
 start = time.time()
 
 left = left_riemann(f, a, n, h)
-right = right_riemann(f, a, n, h)
-middle = middle_riemann(f, a, n, h)
-trap = trapezoid_riemann(f, a, b, n, h)
+# right = right_riemann(f, a, n, h)
+# middle = middle_riemann(f, a, n, h)
+# trap = trapezoid_riemann(f, a, b, n, h)
+total = 0.0
 
-print "Total calculation time: ", time.time() - start
-print "Left value:",left    , "Right:",right    , "Middle:",middle    , "Trapezoidal",trap
+for i in xrange(10):
+	start = time.time()
+	left = left_riemann(f, a, n, h)
+	total += (time.time() - start)
+
+total /= 10.0
+
+print("%.15f") % (total)
+
+# print "Total calculation time: ", time.time() - start
+# print left
+# print "Left value:",left    , "Right:",right    , "Middle:",middle    , "Trapezoidal",trap
