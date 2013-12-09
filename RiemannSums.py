df660f7a4d065ad19450515ce0a417566bec08e7
Michael MacCallum <maccdevteam@gmail.com>
2013-12-09 10:35:24 -0500

    removed camel-case

--- RiemannSums.py
+++ RiemannSums.py
@@ -1,21 +1,21 @@
 import time
 
-def leftRiemann(f,a,n,h):
+def left_riemann(f,a,n,h):
 		
 	return sum((f(i * h + a)) for i in xrange(n)) * h
 	
 	
-def rightRiemann(f,a,n,h):
+def right_riemann(f,a,n,h):
 	
 	return sum((f(i * h + a)) for i in xrange(1, n + 1)) * h
 
-def middleRiemann(f,a,n,h):
+def middle_riemann(f,a,n,h):
 
 	add = h * 0.5 + a
 
 	return sum((f(i * h + add)) for i in xrange(n)) * h
 
-def trapezoidRiemann(f,a,b,n,h):
+def trapezoid_riemann(f,a,b,n,h):
 	
 	s = f(a) + f(b)
 	
@@ -37,11 +37,10 @@ h = (b - a) / float(n)
 
 start = time.time()
 
-left = leftRiemann(f, a, n, h)
-# right = rightRiemann(f, a, n, h)
-# middle = middleRiemann(f, a, n, h)
-# trap = trapezoidRiemann(f, a, b, n, h)
+left = left_riemann(f, a, n, h)
+right = right_riemann(f, a, n, h)
+middle = middle_riemann(f, a, n, h)
+trap = trapezoid_riemann(f, a, b, n, h)
 
 print "Total calculation time: ", time.time() - start
-print left
-# print "Left value:",left    , "Right:",right    , "Middle:",middle    , "Trapezoidal",trap
+print "Left value:",left    , "Right:",right    , "Middle:",middle    , "Trapezoidal",trap
