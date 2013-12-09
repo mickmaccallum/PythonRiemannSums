49fb5c7fee428fdb482558e698e01ee4d31d664e
Michael MacCallum <maccdevteam@gmail.com>
2013-12-09 11:00:56 -0500

    Going to revisit the 'old way' as an option. After doing some profiling, it seems to be able to maintain accuracy, and its while loop manages to match the for i in xrange speed

--- oldWay.py
+++ oldWay.py
@@ -2,11 +2,10 @@ import time
 
 def leftSum(f,a,b,n):
 	h = (b - a) / n
-	print h
 	i = a
 	total = 0.0
 
-	while i <= (b - h):
+	while i <= b:
 		total += f(i)
 		i += h
 
@@ -15,10 +14,15 @@ def leftSum(f,a,b,n):
 
 f = lambda x:5*x
 
-start = time.time()
 
-left = leftSum(f,-3.0,100.0,20000.0)
 
-print time.time() - start
+total = 0.0
 
-print left
+for i in xrange(10):
+	start = time.time()
+	left = leftSum(f,-3.0,100.0,20000.0)
+	total += (time.time() - start)
+
+total /= 10.0
+
+print("%.15f") % (total)
