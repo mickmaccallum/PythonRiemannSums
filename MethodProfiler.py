01b993d27a8a77e777dd1ca165bf17305d5b1ac2
Michael MacCallum <maccdevteam@gmail.com>
2013-12-11 09:18:21 -0500

    What?

--- MethodProfiler.py
+++ MethodProfiler.py
@@ -30,43 +30,10 @@ def n_right_incrementer():
 def h_right_incrementer():
 	
 
-def run_profiler_loops_1():
-	t1 = 0.0
-	for i in xrange(int(r)):
-		s = time()
-		l = n_left_incrementer(f,a,n,h)
-		t1 += time() - s
-	return t1
-
-def run_profiler_loops_2():
-	t2 = 0.0
-	for i in xrange(int(r)):
-		s = time()
-		l = h_left_incrementer(f,a,b,n,h)
-		t2 += time() - s
-	return t2
-	
 
 def profile_left_summation():
 
-	t1 = run_profiler_loops_1()
-	t2 = run_profiler_loops_2()
-
-	t1 /= r
-	t2 /= r
-
-	line = "\n"
-
-	txt = open("ProfilerOutput.txt", "w")
-
-	print >> txt, time()
-	print >> txt, "Left Sum Times" + line
-	print >> txt, ("n_left_incrementer: %s" % t1)
-	print >> txt, ("h_left_incrementer: %s" % t2) + line
-	print >> txt, ("Difference: %s" % fabs(t1 - t2))
-
-	txt.close()
 
 
 
-profile_left_summation()
+# profile_left_summation()
